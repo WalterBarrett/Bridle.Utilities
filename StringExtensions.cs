6f66180cbf4be67a71885d41dfa2f503144c3c2f
Walter Barrett <WalterBarrett@users.noreply.github.com>
2024-07-10 18:20:02 -0400

    Remove no-longer-relevant string extension methods.

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -6,35 +6,10 @@
  */
 #endregion
 
-using System;
-using static System.String;
-
 namespace Bridle.Utilities
 {
     public static class StringExtensions
     {
-        /// <summary>
-        /// Provides a more natural way to call String.Format() on a string.
-        /// </summary>
-        /// <param name="args">An object array that contains zero or more objects to format</param>
-        public static string FormatWith(this string s, params object[] args)
-        {
-            return s == null ? null : Format(s, args);
-        }
-
-        /// <summary>
-        /// Provides a more natural way to call String.Format() on a string.
-        /// </summary>
-        /// <param name="provider">An object that supplies the culture specific formatting</param>
-        /// <param name="args">An object array that contains zero or more objects to format</param>
-        public static string FormatWith(this string s, IFormatProvider provider, params object[] args)
-        {
-            return s == null ? null : Format(provider, s, args);
-        }
-
-        public static bool IsNullOrWhitespace(this string input)
-        {
-            return IsNullOrWhiteSpace(input);
-        }
+        public static bool IsNullOrWhitespace(this string input) => string.IsNullOrWhiteSpace(input);
     }
 }
